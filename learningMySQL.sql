88f8653a95d1fa340952940e035188eb9c8cc943
HamiltonWhite1 <92882456+HamiltonWhite1@users.noreply.github.com>
2022-04-05 12:38:11 -0400

    More MySQL exercises completed

---
+++ learningMySQL.sql
@@ -0,0 +1,24 @@
+-- SELECT *
+-- from order_items
+-- WHERE order_id = 6 AND unit_price * quantity > 30
+
+-- SELECT *
+-- FROM customers
+-- WHERE NOT birth_date >= '1990-01-01' OR 
+-- 	(points > 1000 AND state = 'VA')
+
+-- SELECT *
+-- FROM Customers
+-- WHERE birth_date > '1990-01-01'
+
+-- SELECT name, unit_price, unit_price * 1.1 AS new_price
+-- FROM products
+
+-- SELECT DISTINCT state
+-- FROM customers
+
+-- SELECT 
+-- 	first_name, 
+--     last_name, 
+--     points + 10 AS discount_factor
+-- FROM customers
