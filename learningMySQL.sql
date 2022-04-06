92a852fa685dabaaff89e431fae9c332f2a2093d
HamiltonWhite1 <92882456+HamiltonWhite1@users.noreply.github.com>
2022-04-06 13:37:52 -0400

    More learning of MySQL

--- learningMySQL.sql
+++ learningMySQL.sql
@@ -1,3 +1,28 @@
+SELECT *
+FROM sql_store.customers
+WHERE last_name REGEXP 'field'
+
+
+-- SELECT *
+-- FROM sql_store.customers
+-- WHERE phone LIKE '%9'
+
+-- SELECT * 
+-- FROM sql_store.customers
+-- WHERE last_name LIKE 'B____y'
+
+-- SELECT *
+-- FROM sql_store.customers
+-- WHERE birth_date BETWEEN '1990-01-01' AND '2000-01-01'
+
+-- SELECT *
+-- FROM sql_store.products
+-- WHERE quantity_in_stock IN (49, 38, 72)
+
+-- SELECT * 
+-- FROM Customers
+-- WHERE state NOT IN ('VA', 'GA', 'FL')
+
 -- SELECT *
 -- from order_items
 -- WHERE order_id = 6 AND unit_price * quantity > 30
